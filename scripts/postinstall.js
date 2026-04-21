3ec9c48ceb70f67f89acbf48c7805042a2fcf501
Yusuf Demirci <yusuf@nefarius.co>
2026-04-21 20:31:06 +0300

    Edit: remove playwright

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,1 @@
 #!/usr/bin/env node
-import { spawnSync } from "child_process";
-
-const result = spawnSync("npx", ["playwright", "install", "chromium"], {
-  stdio: ["ignore", process.stderr, process.stderr],
-});
-
-process.exit(result.status ?? 0);
